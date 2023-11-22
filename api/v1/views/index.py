ad913991400d3093c9254236d94989888e9dd350
MahmoudEasa <MahmoudEasa@gmail.com>
2023-11-22 14:47:27 +0000

    Login API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,99 +4,32 @@ from models.user import User
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
+from hashlib import md5
 
 
-@app_views.route('/login', methods=['Post'], strict_slashes=False)
-def get_indexs():
-    """
-    Retrieves the list of all index objects
-    or a specific index
-    """
-    all_indexs = storage.all(User).values()
-    list_indexs = []
-    for index in all_indexs:
-        list_indexs.append(index.to_dict())
-
-    return (jsonify(list_indexs))
-
-
-@app_views.route('/indexs/<index_id>', methods=['GET'], strict_slashes=False)
-def get_index(index_id):
+@app_views.route('/login', methods=['POST'], strict_slashes=False)
+def get_index():
     """ Retrieves an index """
-    index = storage.get(User, index_id).first().to_dict()
-    if not index:
-        abort(404)
 
-    return jsonify(index)
-
-
-@app_views.route('/indexs/<index_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def delete_index(index_id):
-    """
-    Deletes a index Object
-    """
-
-    index = storage.get(User, index_id).first()
-
-    if not index:
-        abort(404)
-
-    storage.delete(index)
-    storage.save()
-
-    return make_response(jsonify({'message': 'User deleted successfully'}), 200)
-
-
-@app_views.route('/indexs', methods=['POST'], strict_slashes=False)
-def post_index():
-    """
-    Creates a index
-    """
     if not request.get_json():
         abort(400, description="Not a JSON")
 
-    if 'indexname' not in request.get_json():
-        abort(400, description="Missing indexname")
     if 'email' not in request.get_json():
         abort(400, description="Missing email")
     if 'password' not in request.get_json():
         abort(400, description="Missing password")
-    if 'phone' not in request.get_json():
-        abort(400, description="Missing phone")
-    if 'address' not in request.get_json():
-        abort(400, description="Missing address")
-    if 'is_admin' not in request.get_json():
-        abort(400, description="Missing is_admin")
+ 
+    all_users = storage.all(User).values()
 
-    data = request.get_json()
-    instance = User(**data)
-    instance.save()
-    index = instance.to_dict()
-
-    return make_response(jsonify(index), 201)
-
-
-@app_views.route('/indexs/<index_id>', methods=['PUT'], strict_slashes=False)
-def put_index(index_id):
-    """
-    Updates a index
-    """
-    index = storage.get(User, index_id).first()
-
-    if not index:
+    if not all_users:
         abort(404)
 
-    if not request.get_json():
-        abort(400, description="Not a JSON")
+    req = request.get_json()
+    hash_password = md5(req['password'].encode()).hexdigest()
 
-    ignore = ['id', 'created_at', 'updated_at']
+    for user in all_users:
+        if user.email == req['email'] and\
+           user.password == hash_password:
+            return (user.to_dict())
 
-    data = request.get_json()
-    for key, value in data.items():
-        if key not in ignore:
-            setattr(index, key, value)
-    storage.save()
-    index = index.to_dict()
-
-    return make_response(jsonify(index), 200)
+    return (make_response(jsonify({'message': 'Something is wrong'}), 400))
